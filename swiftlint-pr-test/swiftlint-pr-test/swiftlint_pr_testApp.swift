6dd7e3a7e42efa3820a81cd77683636ac11a1c8b
Michal Bínovský <michal@binovsky.com>
2026-01-26 17:12:00 +0100

    initial commit

---
+++ swiftlint-pr-test/swiftlint-pr-test/swiftlint_pr_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftlint_pr_testApp.swift
+//  swiftlint-pr-test
+//
+//  Created by Michal Bínovský on 26.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLintPRTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
