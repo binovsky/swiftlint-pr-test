0d86aa0ac06b503380c7aeb12929e01399b17798
Michal Bínovský <michal@binovsky.com>
2026-01-27 08:17:51 +0100

    introduce additional warnings

--- swiftlint-pr-test/swiftlint-pr-test/ContentView.swift
+++ swiftlint-pr-test/swiftlint-pr-test/ContentView.swift
@@ -11,12 +11,18 @@ struct ContentView: View {
     var body: some View {
         VStack {
             
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
         .padding()
+    }
+    
+    func testFunction( ) -> Int { // extra spaces
+        let xxx=1 // no spaces around =
+        return xxx
     }
 }
 
